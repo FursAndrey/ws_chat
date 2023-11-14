9c338ca38df7c9ed61577448852e857aab3507ab
Furs Andrei <you@example.com>
2023-11-14 13:05:16 +0300

    add chat page

---
+++ resources/js/Pages/Chat/Index.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="flex">
+        <div class="w-1/2 p-4 mr-2 border border-sky-800">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur eius necessitatibus cum, voluptatum laudantium doloribus molestiae, harum consequatur animi accusantium a? Similique ipsam, quo nostrum saepe fugit sed molestiae praesentium.
+        </div>
+        <div class="w-1/2 p-4 ml-2 border border-sky-800">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tenetur eius necessitatibus cum, voluptatum laudantium doloribus molestiae, harum consequatur animi accusantium a? Similique ipsam, quo nostrum saepe fugit sed molestiae praesentium.
+        </div>
+    </div>
+</template>
+
+<script>
+import My from '@/Layouts/My.vue';
+export default {
+    name: 'Index',
+    layout: My
+}
+</script>
+
+<style>
+
+</style>
